8f5787cb74e73b4dc3c2bb10a0fae0f65255c6de
Matvey <matvexa333@gmail.com>
2021-07-20 13:50:35 +0300

    create project

---
+++ MoviesDB/MoviesDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesDB
+//
+//  Created by Матвей Кашин on 20.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
