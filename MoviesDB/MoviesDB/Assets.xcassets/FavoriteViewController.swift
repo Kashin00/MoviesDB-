02811a7dc7c965e0f6964d05f0c619583ecd0e82
Matvey <matvexa333@gmail.com>
2021-07-20 16:29:09 +0300

    create storyboard

---
+++ MoviesDB/MoviesDB/Assets.xcassets/FavoriteViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoriteViewController.swift
+//  MoviesDB
+//
+//  Created by Матвей Кашин on 20.07.2021.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     
+    }
+}
